f530c18896392ddc790462ea81a79f13b588d065
Marcus Svensson <marcussvensson92@live.se>
2015-09-09 13:40:26 +0200

    Added pool allocator + test

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,7 +1,11 @@
 #include "FrameAllocator.h"
+#include "PoolAllocator.h"
 #include <new>
+#include <iostream>
+#include <chrono>
 
 #define TEST_FRAME_ALLOCATOR 1
+#define TEST_POOL_ALLOCATOR 1
 
 struct DebugStruct
 {
@@ -10,6 +14,8 @@ struct DebugStruct
 	bool	Alpaca;
 	int		NumberOfLegs;
 };
+
+void testPoolAllocator();
 
 int main() 
 {
@@ -34,5 +40,47 @@ int main()
 
 	FrameAllocator::Shutdown();
 #endif
+
+#if TEST_POOL_ALLOCATOR == 1
+	testPoolAllocator();
+#endif
+
     return 0;
+}
+
+void testPoolAllocator()
+{
+	const unsigned int ALLOCATIONS = 100000;
+
+	std::chrono::steady_clock::time_point start, end;
+	long long duration;
+	DebugStruct* debugStructArray[ALLOCATIONS];
+
+	start = std::chrono::high_resolution_clock::now();
+	for (size_t i = 0; i < ALLOCATIONS; ++i)
+	{
+		debugStructArray[i] = new DebugStruct(true, 5);
+	}
+	for (size_t i = 0; i < ALLOCATIONS; ++i)
+	{
+		delete debugStructArray[i];
+	}
+	end = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast< std::chrono::milliseconds >(end - start).count();
+	std::cout << "Allocation test WITHOUT pool allocator: " << duration << " ms" << std::endl;
+
+	start = std::chrono::high_resolution_clock::now();
+	PoolAllocator* poolAllocator = new PoolAllocator(32, ALLOCATIONS);
+	for (size_t i = 0; i < ALLOCATIONS; ++i)
+	{
+		debugStructArray[i] = new(poolAllocator->Allocate<DebugStruct>()) DebugStruct(true, 5);
+	}
+	for (size_t i = 0; i < ALLOCATIONS; ++i)
+	{
+		poolAllocator->Deallocate(debugStructArray[i]);
+	}
+	delete poolAllocator;
+	end = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast< std::chrono::milliseconds >(end - start).count();
+	std::cout << "Allocation test WITH pool allocator: " << duration << " ms" << std::endl;
 }
