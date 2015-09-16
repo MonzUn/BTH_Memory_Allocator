194a68f38a4d637e73b19bd165f6775da9e03806
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-09-16 23:51:00 +0200

    Added default value defines for Initialize parameters on frameAllocator

--- src/core/FrameAllocator.h
+++ src/core/FrameAllocator.h
@@ -6,10 +6,13 @@
 
 #define FRAME_ALLOCATOR_DEBUG 1
 
+#define FRAME_ALLOCATOR_DEFAULT_MEMORY_BYTE_SIZE 1 * MEBI
+#define FRAME_ALLOCATOR_DEFAULT_ALIGNMENT 8
+
 class FrameAllocator
 {
 public:
-	void Initialize( size_t memoryByteSize = 1 * MEBI, size_t alignment = 16 )
+	void Initialize( size_t memoryByteSize = FRAME_ALLOCATOR_DEFAULT_MEMORY_BYTE_SIZE, size_t alignment = FRAME_ALLOCATOR_DEFAULT_ALIGNMENT )
 	{
 		assert( !mInitialized );
 
